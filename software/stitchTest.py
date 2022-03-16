b1137ac30fc8a9339cecdf3b33852ceafb174569
deguzmanc <deguzmanc@ufl.edu>
2022-03-16 17:53:48 -0400

    funciton for stitching

--- software/stitchTest.py
+++ software/stitchTest.py
@@ -16,31 +16,41 @@ import cv2
 # # grab the paths to the input images and initialize our images list
 # print("[INFO] loading images...")
 # imagePaths = sorted(list(paths.list_images(args["images"])))
-imagePaths = ['testImage0.png','testImage1.png','testImage2.png']
-images = []
+# imagePaths = ['testImage0.png','testImage1.png','testImage2.png']
+# images = []
 
-# loop over the image paths, load each one, and add them to our
-# images to stitch list
-for imagePath in imagePaths:
-	image = cv2.imread(imagePath)
-	images.append(image)
+# # loop over the image paths, load each one, and add them to our
+# # images to stitch list
+# for imagePath in imagePaths:
+# 	image = cv2.imread(imagePath)
+# 	images.append(image)
 
-print(images[0].shape)
+# print(images[0].shape)
+
 # initialize OpenCV's image stitcher object and then perform the image
 # stitching
-print("[INFO] stitching images...")
-stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
-(status, stitched) = stitcher.stitch(images)
 
-# if the status is '0', then OpenCV successfully performed image
-# stitching
-if status == 0:
-	# write the output stitched image to disk
-	cv2.imwrite('stitchedImages.png', stitched)
-	# display the output stitched image to our screen
-	cv2.imshow("Stitched", stitched)
-	cv2.waitKey(0)
-# otherwise the stitching failed, likely due to not enough keypoints)
-# being detected
-else:
-	print("[INFO] image stitching failed ({})".format(status))
+def StitchImages(images):
+	'''
+	Inputs:
+	images: list of numpy arrays (mxnx3)
+	'''
+	stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
+	(status, stitched) = stitcher.stitch(images)
+
+	# if the status is '0', then OpenCV successfully performed image
+	# stitching
+	if status == 0:
+		print("[INFO] stitching images...")
+		# write the output stitched image to disk
+		# cv2.imwrite('stitchedImages.png', stitched)
+		# # display the output stitched image to our screen
+		# cv2.imshow("Stitched", stitched)
+		# cv2.waitKey(0)
+	# otherwise the stitching failed, likely due to not enough keypoints)
+	# being detected
+	else:
+		print("[INFO] image stitching failed ({})".format(status))
+
+	return stitched
+	
