a63c4ae456a6deb7c25579a57cd02085903d79ff
deguzmanc <deguzmanc@ufl.edu>
2022-03-30 16:57:55 -0400

    updated video_loop

--- software/video_loop.py
+++ software/video_loop.py
@@ -1,37 +1,48 @@
-# https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
-
 import numpy as np
 import cv2 as cv
 
-# from stitchTest import StitchImages
+num_cameras = 3
+cameras = [cv.VideoCapture(i) for i in range(num_cameras)]
 
-# cameras = []
+for i, camera in enumerate(cameras):
+    if not camera.isOpened():
+        print(f"Cannot open camera {i}")
+        exit()
 
-camera_0 = cv.VideoCapture(0)
-if not camera_0.isOpened():
-    print("Cannot open camera")
-    exit()
+print("Press 'q' to quit")
+print("Press 'c' to save images and quit")
 
-index = 0
 while True:
     # Capture frame-by-frame
-    ret, frame = camera_0.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # frames = [camera.read()[1] for camera in cameras]
+    frames = []
+    for camera in cameras:
+        ret, frame = camera.read()
+        frames.append(frame)
+        if not ret:
+            print("Can't receive frame. Exiting ...")
+            break
+
+    # Our operations on the frame come here (middleware)
+
+    # stitcher = cv.Stitcher_create()  # if imutils.is_cv3() else cv.Stitcher_create()
+    # (status, stitched) = stitcher.stitch([frame, frame2])
+
+    frames = np.concatenate(frames, axis=1) # change to stitcher
 
     # Display the resulting frame
-    cv.imshow('frame', frame)
+    cv.imshow('frame', frames)
     if cv.waitKey(1) == ord('q'):
         break
 
     if cv.waitKey(1) == ord('c'):
-        cv.imwrite('capture' + str(index) + '.png', frame)
-        print('Image saved')
-        index = index + 1
+        for i, frame in enumerate(frames):
+            cv.imwrite(f'capture{i}.png', frame)
+            print('Image saved')
+        break
+
 # When everything done, release the capture
-camera_0.release()
+for camera in cameras:
+    camera.release()
+
 cv.destroyAllWindows()
