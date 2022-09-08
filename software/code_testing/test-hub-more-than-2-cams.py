97ca162b236acfcfbecc576bfa2c9c85b9059b22
will-kirby <wkirby01@gmail.com>
2022-09-08 11:16:24 -0400

    Gooo

--- software/code_testing/test-hub-more-than-2-cams.py
+++ software/code_testing/test-hub-more-than-2-cams.py
@@ -7,11 +7,13 @@
 import cv2 as cv
 import numpy as np
 
-compress = 1 # change whether to use the compressed camera feed
-startIndex = 1 # if on laptop, avoid the webcam (0 for jetson)
-
 cameras = []
 num_cameras = 4 # on my laptop, this worked, used webcam, 2 hub, and 1 sep plugged in
+startIndex = 0 # if on laptop, avoid the webcam (0 for jetson)
+compress = 0 # change whether to use the compressed camera feed
+
+print("Staring program...")
+print(f"Number of cameras={num_cameras}, Starting Index={startIndex}, Compress camera feed={compress}")
 for i in range(num_cameras):
     camera = cv.VideoCapture(i+startIndex)
     if compress:
@@ -25,11 +27,14 @@ for i in range(num_cameras):
     cameras.append(camera)
 
     #cameras = [cv.VideoCapture(i) for i in range(num_cameras)]
+print("Cameras constructed")
 
 for i, camera in enumerate(cameras):
     if not camera.isOpened():
         print(f"Cannot open camera {i}")
         exit()
+print(f"{num_cameras} successfully opened")
+
 
 # print("Press 'c' to capture image and quit")
 print("Press 'q' to quit")
@@ -53,9 +58,10 @@ while True:
 
     # calculate pushed (shape[0] is height, shape[1] is width)
     pushedImages = np.zeros((max([frame.shape[0] for frame in frames]), sum([frame.shape[1] for frame in frames]), 3), dtype="uint8" )
+    
     currentWidth = 0
     for i, frame in enumerate(frames):
-        pushedImages[0:frame.shape[0], (currentWidth-shiftAmount*i):(frame.shape[1]-shiftAmount*i)] = frame
+        pushedImages[0:frame.shape[0], (currentWidth-shiftAmount*i):(currentWidth + frame.shape[1]-shiftAmount*i)] = frame
         currentWidth += (frame.shape[1] - shiftAmount)
     cv.imshow('pushed images', pushedImages)
 
