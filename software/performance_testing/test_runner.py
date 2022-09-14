c018116ce2458471df5163575547cccfb51c46ec
Dan1004x <danielipark99@gmail.com>
2022-09-14 18:17:41 -0400

    Changes to log arguments
    Defaults are displayed when using help.
    Added arguments to allow selection of stitcher and naming the log.

--- software/performance_testing/test_runner.py
+++ software/performance_testing/test_runner.py
@@ -14,7 +14,7 @@ sys.path.insert(1, '../common/')
 # import open_camera
 
 # put number of cameras, stitching algo
-def run_test(duration, log_name, num_cameras=4, stitching_algo='OpenCV Stitcher'):
+def run_test(duration, log_name, num_cameras, algo): # Small change since all parameters can be positional
   """
   Runs test for a user specified amount of time
   Input:
@@ -26,7 +26,7 @@ def run_test(duration, log_name, num_cameras=4, stitching_algo='OpenCV Stitcher'
 
   with open(log_name, "w", newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow([f"Duration: {duration}", f"Cameras: {num_cameras}", f"Stitching Algorithm: {stitching_algo}"])
+    writer.writerow([f"Duration: {duration}", f"Cameras: {num_cameras}", f"Stitching Algorithm: {algo}"])
 
     fieldnames = ['Start Time', 'End Time', 'Status']
     writer.writerow(fieldnames)
@@ -58,21 +58,34 @@ def run_test(duration, log_name, num_cameras=4, stitching_algo='OpenCV Stitcher'
   return
 
 if __name__ == '__main__' :
-  parser = argparse.ArgumentParser(description='Run testing for given amount of time')
-  parser.add_argument('-c', '--cameras', type=int, default=4,
+  # Adding the formatter class will inform users of default values when using -help
+  parser = argparse.ArgumentParser(description='Stitching functionality test with variable camera range and timing', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-c', '--cameras', type=int, default=4, 
     help='Specify the amount of cameras')
   parser.add_argument('-d', '--duration', type=int, default=10,
     help='Specify the duration in seconds to run the test')
+  parser.add_argument('-s', '--stitcher', type=int, default=0, # Stitcher argument. We're using another stitcher for comparison right? I don't know what to put for the 2nd option so I left it as 'other'
+    help='Specify stitcher mode. 0 for OpenCV, 1 for *OTHER*')
   parser.add_argument('-i', '--image', action='store_true', default=False,
     help='Flag to take one image')
+  parser.add_argument('-n', '--name', type=str, default='current time', 
+    help='Specify a name for the created log')
 
   args = parser.parse_args()
   duration = args.duration
 
   dirname = os.path.dirname(__file__)
-  formatted_time = time.strftime("%Y-%m-%d_%H.%M.%S")
-  log_name = os.path.join(dirname, f'logs/{formatted_time}.csv')
-
+  if (args.name == 'current time'):
+    name = time.strftime("%Y-%m-%d_%H.%M.%S")
+  else:
+    name = args.name
+  log_name = os.path.join(dirname, f'logs/{name}.csv')
   image_dir = os.path.join(dirname, 'images')
 
-  run_test(duration, log_name, args.cameras)
+  # Will have to a similar if statement when selecting stitcher once stitching is integrated.
+  if (args.stitcher):
+    algo = 'OpenCV Stitcher'
+  else:
+    algo = 'OTHER'
+
+  run_test(duration, log_name, algo, args.cameras)
