1d45fa227a9c694586b8421b915e279afd76498c
deguzmanc <deguzmanc@ufl.edu>
2022-09-26 16:09:56 -0400

    images for erman

--- software/functionality_testing/test-hub-more-than-2-cams.py
+++ software/functionality_testing/test-hub-more-than-2-cams.py
@@ -8,10 +8,12 @@ import cv2 as cv
 import numpy as np
 
 cameras = []
-num_cameras = 3# on my laptop, this worked, used webcam, 2 hub, and 1 sep plugged in
+
+num_cameras = 6# on my laptop, this worked, used webcam, 2 hub, and 1 sep plugged in
 startIndex = 0 # if on laptop, avoid the webcam (0 for jetson)
-compress = 1 # change whether to use the compressed camera feed
-reverseCams = 0 # reverse the camera order
+compress = True # change whether to use the compressed camera feed
+reverseCams = 1 # reverse the camera order
+
 newheight = 480
 newWidth = 640
 shiftAmount = 56
@@ -20,7 +22,8 @@ shiftAmount = 56
 print("Staring program...")
 print(f"Number_of_cameras={num_cameras}, Starting_Index={startIndex}, Compress_camera_feed={compress}, Reverse_Cameras={reverseCams}")
 for i in range(num_cameras):
-    camera = cv.VideoCapture("/dev/camera"+str(i+startIndex))
+    camera = cv.VideoCapture(f'/dev/camera{i}')
+    
     if compress:
         camera.set(cv.CAP_PROP_FPS, 15)
         camera.set(cv.CAP_PROP_FRAME_WIDTH, 320)
@@ -28,7 +31,7 @@ for i in range(num_cameras):
         camera.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M', 'J', 'P', 'G'))
     else:
         camera.set(3, 200)# width
-
+    
     cameras.append(camera)
 #cameras = [cv.VideoCapture(i) for i in range(num_cameras)]
 
@@ -57,7 +60,9 @@ while True:
     # Capture frame-by-frame
     for i, camera in enumerate(cameras):
         ret, frame = camera.read()
-       # frame = cv.resize(frame, dsize=(newheight, newWidth), interpolation=cv.INTER_LINEAR) # make it bigger
+
+
+        frame = cv.resize(frame, dsize=(newheight, newWidth), interpolation=cv.INTER_LINEAR) # make it bigger
         frames.append(frame)
         # if frame is read correctly ret is True
         if not ret:
@@ -81,10 +86,12 @@ while True:
         break
 
     if cv.waitKey(1) == ord('c'):
-         for i, frame in enumerate(frames):
-             cv.imwrite(f'capture{i}.png', frame)
-             print(f'capture{i}.png saved')
-         break
+
+        print(len(frames))
+        for i, frame in enumerate(frames):
+           cv.imwrite(f'capture{i}.png', frame)
+           print(f'capture{i}.png saved')
+        break
 
    # if cv.waitKey(1) == ord('s'):
     #    shiftAmount += 2
