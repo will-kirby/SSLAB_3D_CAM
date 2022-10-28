f583244319028d04f353949818b9feecfdf48308
will-kirby <wkirby01@gmail.com>
2022-10-28 16:50:44 -0400

    Code for cyldinrical stitching and testing file

--- software/CameraSystemClass.py
+++ software/CameraSystemClass.py
@@ -8,6 +8,7 @@ class CameraSystem:
     homographyMatrix = None
     overlapAmount = 56
     homographyMatrix = []
+    warpAmount = 200
     
 
     def __init__(self, cameraIndexList=[], compressCameraFeed=True, useLinuxCam=True):
@@ -37,7 +38,7 @@ class CameraSystem:
         for camera in self.cameras:
             camera.release()
 
-    def captureCameraImages(self, newResizedDimensions = None, interpolationType = cv.INTER_LINEAR):
+    def captureCameraImages(self, cylWarp = False, newResizedDimensions = None, interpolationType = cv.INTER_LINEAR):
         # newDimensions is a tuple - (newheight, newWidth)
         # interpolationType is only used if newDimensions is set
         # returns None if error, camera frames otherwise
@@ -53,7 +54,10 @@ class CameraSystem:
                 frame = cv.resize(frame, dsize=newResizedDimensions, interpolation=interpolationType)
 
             frames.append(frame)
-        
+
+        if cylWarp:
+            frames = self.cylWarpFrames(frames)
+    
         return frames
 
     def writeFramesToFile(self, frames, baseName="cameraImage", appendIndex=True, extension="jpg"):
@@ -263,15 +267,221 @@ class CameraSystem:
             
         return img
 
+    def cylindricalWarp(self, img, K):
+        """This function returns the cylindrical warp for a given image and intrinsics matrix K"""
+        
+        h_,w_ = img.shape[:2]
+
+        # pixel coordinates
+        y_i, x_i = np.indices((h_,w_)) # does [[0,0,0...],[1,1,1...],[n-1,n-1,n-1...] then [0,1,2,3...n-1] * n
+        X = np.stack([x_i,y_i,np.ones_like(x_i)],axis=-1).reshape(h_*w_,3) # to homog -> stacks [0,1,2,3] vert , then [0,0,0,], then [1,1,1,1]
+        Kinv = np.linalg.inv(K) 
+        X = Kinv.dot(X.T).T # normalized coords (K^-1 * X^T)^T
+
+        # calculate cylindrical coords (sin\theta, h, cos\theta)
+        A = np.stack([np.sin(X[:,0]),X[:,1],np.cos(X[:,0])],axis=-1).reshape(w_*h_,3)
+        B = K.dot(A.T).T # project back to image-pixels plane
+
+        # back from homog coords
+        B = B[:,:-1] / B[:,[-1]]
+
+        # make sure warp coords only within image bounds
+        B[(B[:,0] < 0) | (B[:,0] >= w_) | (B[:,1] < 0) | (B[:,1] >= h_)] = -1
+        B = B.reshape(h_,w_,-1)
+        # img_rgba = cv.cvtColor(img,cv.COLOR_BGR2BGRA) # for transparent borders...
+
+        # warp the image according to cylindrical coords
+        
+        # return cv.remap(img_rgba, B[:,:,0].astype(np.float32), B[:,:,1].astype(np.float32), cv.INTER_AREA, borderMode=cv.BORDER_TRANSPARENT)
+        return cv.remap(img, B[:,:,0].astype(np.float32), B[:,:,1].astype(np.float32), cv.INTER_AREA) #, borderMode=cv.BORDER_TRANSPARENT)
+
+    def cylWarpFrames(self, frames):
+        warpedFrames = []
+        # warpAmount = self.warpAmount
+        warpAmount = 200
+        for frame in frames:
+            h, w = frame.shape[:2]
+            K = np.array([[warpAmount,0,w/2],[0,warpAmount,h/2],[0,0,1]]) # mock intrinsics (normally from camera calibration?)
+            warpedFrames.append(self.cylindricalWarp(frame, K))
+
+            warpAmount -= 5 # attempting to adjust warp amount to be more as images increase, as with 
+
+        # return [self.cylindricalWarp(frame, K) for frame in frames]
+        return warpedFrames
+
+    def overlapImgs(self, imgLeft, imgRight):
+        """
+        imgLeft is the smaller dimension image -> will get resized to larger
+        imgRight is getting imageLeft layed on top of it
+
+        """
+
+        # resize imageLeft -> is there an opencv function to do this faster?
+        # something like resize but to add a black border instead of change the whole thing
+        imgL = np.zeros_like(imgRight)
+        imgL[0:imgLeft.shape[0], 0:imgLeft.shape[1]] = imgLeft
+
+        maskRight = 1 - (np.sum(imgL,axis=-1) > 0) # true if value not (0,0,0)
+        maskRight = np.dstack((maskRight, maskRight, maskRight)) # make one for each pixcel channel
+        imgRightMasked = imgRight * maskRight
+        
+        overlapped = cv.addWeighted(imgL, 1, imgRightMasked, 1, 0, dtype=cv.CV_8U)
+
+        return overlapped
+
+    def calcHomographyWarped(self, frames, saveHomo = True, fileName="fallbackHomography.npy"):
+        """ 
+        Pass in cyl-warped frames. Ends up stitching them together to find keypoints and calculate homography matrices
+        returns a list of the homography matrices
+
+        Accounting for failure:
+            if keypoint matching fails, then stops, as it needs the previous result to keep going
+
+            Possible alternative is to preload the default matrices, if one of the matches fails fall back on the default
+            for that single failure?
+        """
+
+        panoImg = frames[0]
+        homoList = []
+        for i in range(1,len(frames)):
+
+            # find key points
+            kp, des = self._findKPandDesMultiple([frames[i], panoImg])
+            goodMatches = self.matchDescriptors(des[0], des[1])
+            H, matchesMask = self.findHomographyFromMatched(goodMatches, kp[0], kp[1])
+
+            # use H to stitch, then loop and find keypoints again with larger image
+            if H is not None:
+                panoImg = self._stitchWarpedSegment(panoImg, frames[i], H)
+            else:
+                print(f"Error finding keypoints with image {i} and panoImage")
+                return None
+            homoList.append(H)
+
+        if saveHomo:
+            print(f"Saving homo Hleft, Hright to file {fileName}")
+            self.saveHomographyToFile(homoList,fileName)
+
+        return homoList
+
+
+
+    def _stitchWarpedSegment(self, mainPanoImg, newRightSegment, homographyMat):
+        dst = cv.warpPerspective(newRightSegment,homographyMat,(mainPanoImg.shape[1] + newRightSegment.shape[1], newRightSegment.shape[0])) # warp the first image
+        return self.overlapImgs(mainPanoImg, dst)
+
+
+    def stitchWarped(self, frames, homographyList):
+        """
+        Takes in cyl-warped frames, Homography matricies 0-4 (5 matrices for 6 frames)
+        returns a pano of the frames
+        """
+        stitchedImage = None
+        if len(frames)-1 == len(homographyList):
+            stitchedImage = frames[0]
+            
+            for i in range(1,len(frames)):
+                stitchedImage = self._stitchWarpedSegment(stitchedImage, frames[i], homographyList[i-1])
+
+
+        return stitchedImage
+
+
+
+def showTwoImgs(img1, img2):
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(img1)
+    plt.subplot(122)
+    plt.imshow(img2)
+    plt.show()
 
 
 
 
 if __name__ == "__main__":
+    
     print("Constructing camera system")
     # cam = CameraSystem([0],False)
     # cam = CameraSystem([0,1],compressCameraFeed=False)
     cam = CameraSystem([],compressCameraFeed=False)
+
+    # img = cv.imread("functionality_testing/capture3.png")
+    # if img is None:
+    #     print("Bad read")
+    #     exit() 
+
+    frames = cam.readFramesFromFiles(["capture3.png", "capture4.png"],"../images/lab5/")
+
+    img = frames[1]
+    h, w = img.shape[:2]
+    warpAmount = 200 #200 # this should probably come from camera calibration, but whatever
+    K = np.array([[warpAmount,0,w/2],[0,warpAmount,h/2],[0,0,1]]) # mock intrinsics (normally from camera calibration?)
+    img_cyl1 = cam.cylindricalWarp(img, K)
+
+    img = frames[0]
+    h, w = img.shape[:2]
+    K = np.array([[warpAmount,0,w/2],[0,warpAmount,h/2],[0,0,1]]) # mock intrinsics (normally from camera calibration?)
+    img_cyl0 = cam.cylindricalWarp(img, K)
+
+    img_cyl0_1 = cam.cylindricalWarp(img_cyl0, K)
+
+    showTwoImgs(img_cyl0, img_cyl0_1)
+
+    warpAmount = 200 #200 # this should probably come from camera calibration, but whatever
+    K = np.array([[warpAmount,0,w/2],[0,warpAmount,h/2],[0,0,1]]) # mock intrinsics (normally from camera calibration?)
+    img_cyl0_2 = cam.cylindricalWarp(img, K)
+
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(img_cyl0)
+
+    plt.subplot(132)
+    plt.imshow(img_cyl0_1)
+
+    plt.subplot(133)
+    plt.imshow(img_cyl0_2)
+    plt.show()
+    
+    # kp, des = cam._findKPandDesMultiple([frames[0], img_cyl])
+    # kp, des = cam._findKPandDesMultiple([frames[1], frames[0]])
+    kp, des = cam._findKPandDesMultiple([img_cyl1, img_cyl0])
+    goodMatches = cam.matchDescriptors(des[0], des[1])
+    H, matchesMask = cam.findHomographyFromMatched(goodMatches, kp[0], kp[1])
+    # print("Homo:",H)
+
+    if H is not None:
+        # dst = cam.homographyStitch(img_cyl1, img_cyl0, H)
+
+        # plt.figure()
+        # plt.subplot(121)
+        
+        dst = cv.warpPerspective(img_cyl1,H,((img_cyl0.shape[1] + img_cyl1.shape[1]), img_cyl1.shape[0])) # warp the first image
+        # dst = cv.cvtColor(dst,cv.COLOR_BGR2BGRA)
+
+        cam.overlapImgs(img_cyl0, dst)
+        # plt.imshow(dst)
+
+        # alphaMask = (np.sum(img_cyl0,axis=-1) > 0) * 255.0
+        # print("Alpha mask shape",alphaMask.shape)
+        # img_cyl0 = np.dstack((img_cyl0, alphaMask))
+
+        
+    
+        # dst[0:img_cyl0.shape[0], 0:img_cyl0.shape[1]] = img_cyl0 #plop the second down
+        # plt.subplot(122)
+        # plt.imshow(dst)
+        # # plt.show()
+
+        # plt.figure()
+        # plt.imshow(alphaMask)
+        # plt.show()
+
+        # result_1 = blend_non_transparent(face_img, overlay_img)
+
+    else:
+        print("H is none")
+
 
 
     # print("capturing images")
@@ -280,15 +490,17 @@ if __name__ == "__main__":
     # print("Writing to file")
     # cam.writeFramesToFile(frames)
 
-    print("Reading camera images")
+    # print("Reading camera images")
     # frames = cam.readFramesFromFiles(["testImage1.jpg", "testImage0.jpg"],"code_testing/")
-    frames = cam.readFramesFromFiles(["capture0.png", "capture1.png","capture2.png"],"functionality_testing/")
+    # frames = cam.readFramesFromFiles(["capture0.png", "capture1.png","capture2.png"],"functionality_testing/")
+    # frames = cam.readFramesFromFiles(["capture3.png", "capture4.png"],"images/lab_5/")
+
 
     # triple pano
-    Hl, Hr = cam.calibrateMatrixTriple(frames[0], frames[1], frames[2])
-    dst = cam.tripleHomographyStitch(frames[0], frames[1], frames[2], Hl, Hr)
-    plt.imshow(dst)
-    plt.show()
+    # Hl, Hr = cam.calibrateMatrixTriple(frames[0], frames[1], frames[2])
+    # dst = cam.tripleHomographyStitch(frames[0], frames[1], frames[2], Hl, Hr)
+    # plt.imshow(dst)
+    # plt.show()
 
 
 
